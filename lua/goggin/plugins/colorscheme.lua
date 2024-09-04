a3102ece67d89cfbec753c850b72eafa9277b692
joegoggin <joegoggin27@gmail.com>
2024-09-04 15:09:39 -0400

    Change Highlight Color

--- lua/goggin/plugins/colorscheme.lua
+++ lua/goggin/plugins/colorscheme.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         vim.cmd("colorscheme terafox")
 
-        vim.api.nvim_set_hl(0, "Visual", { bg = "#ad5c7c", fg = "#152528" })
+        vim.api.nvim_set_hl(0, "Visual", { bg = "#e85c51", fg = "#152528" })
         vim.api.nvim_set_hl(0, "CursorLine", { bg = "NONE" })
         vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#e85c51", bg = "NONE", bold = true })
     end,
