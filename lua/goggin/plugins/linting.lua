082e347dd7e63a1e21de5cd6301dcf2012c8b247
joegoggin <joegoggin27@gmail.com>
2024-09-04 13:31:34 -0400

    Fix Linting Issue (Only Lint When ESLint Config is Available)

--- lua/goggin/plugins/linting.lua
+++ lua/goggin/plugins/linting.lua
@@ -3,25 +3,34 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     config = function()
         local lint = require("lint")
+        local uv = vim.loop
 
         lint.linters_by_ft = {
-            javascript = { "eslint_d" },
-            typescript = { "eslint_d" },
-            javascriptreact = { "eslint_d" },
-            typescriptreact = { "eslint_d" },
+            javascript = { "eslint" },
+            typescript = { "eslint" },
+            javascriptreact = { "eslint" },
+            typescriptreact = { "eslint" },
         }
 
         local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
+        local function eslint_config_exists()
+            local files = { ".eslintrc", ".eslintrc.json", ".eslintrc.js", ".eslintrc.yaml", ".eslintrc.yml" }
+            for _, file in ipairs(files) do
+                if uv.fs_stat(file) then
+                    return true
+                end
+            end
+            return false
+        end
+
         vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
             group = lint_augroup,
             callback = function()
-                lint.try_lint()
+                if eslint_config_exists() then
+                    lint.try_lint()
+                end
             end,
         })
-
-        vim.keymap.set("n", "<leader>l", function()
-            lint.try_lint()
-        end, { desc = "Trigger linting for current file" })
     end,
 }
