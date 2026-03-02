7c6aa5a98e1c6db3cd7e096aa96fad1e524e9b2d
joegoggin <joegoggin27@gmail.com>
2026-03-02 03:54:07 -0500

    Open Alpha on blank new tabs and guard command availability

--- lua/goggin/core/autocommands.lua
+++ lua/goggin/core/autocommands.lua
@@ -3,9 +3,39 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     command = "set filetype=make",
 })
 
+local function open_alpha_dashboard()
+    if vim.fn.exists(":Alpha") == 2 then
+        vim.cmd("Alpha")
+    end
+end
+
 vim.api.nvim_create_autocmd("FocusGained", {
     callback = function()
         require("goggin.theme").reload()
+    end,
+})
+
+vim.api.nvim_create_autocmd("TabNewEntered", {
+    callback = function()
+        local buf = vim.api.nvim_get_current_buf()
+
+        if vim.bo[buf].filetype == "alpha" then
+            return
+        end
+
+        if vim.bo[buf].buftype ~= "" then
+            return
+        end
+
+        if vim.bo[buf].modified then
+            return
+        end
+
+        if vim.api.nvim_buf_get_name(buf) ~= "" then
+            return
+        end
+
+        open_alpha_dashboard()
     end,
 })
 
@@ -32,7 +62,7 @@ vim.api.nvim_create_autocmd("BufDelete", {
             end
 
             if real == 0 then
-                vim.cmd("Alpha")
+                open_alpha_dashboard()
             end
         end)
     end,
