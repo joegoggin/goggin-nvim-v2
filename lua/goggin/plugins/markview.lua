dce6250cae2024994659222a22dada1e4177706d
joegoggin <joegoggindev@gmail.com>
2026-05-31 23:15:49 -0400

    Fix markdown diff highlighting

--- lua/goggin/plugins/markview.lua
+++ lua/goggin/plugins/markview.lua
@@ -1,6 +1,24 @@
 return {
     "OXY2DEV/markview.nvim",
     lazy = false,
+    opts = {
+        markdown = {
+            code_blocks = {
+                ["diff"] = {
+                    block_hl = function(_, line)
+                        if line:match("^%+") then
+                            return "MarkviewPalette4Bg"
+                        elseif line:match("^%-") then
+                            return "MarkviewPalette1Bg"
+                        else
+                            return "MarkviewCode"
+                        end
+                    end,
+                    pad_hl = "MarkviewCode",
+                },
+            },
+        },
+    },
 
     -- Completion for `blink.cmp`
     -- dependencies = { "saghen/blink.cmp" },
