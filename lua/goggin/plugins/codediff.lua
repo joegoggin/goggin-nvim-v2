ca3ed18c87a3d2087490171a40712f43f5439eac
joegoggin <joegoggindev@gmail.com>
2026-06-02 22:27:21 -0400

    Update git hunk navigation keymaps

--- lua/goggin/plugins/codediff.lua
+++ lua/goggin/plugins/codediff.lua
@@ -67,8 +67,8 @@ return {
             view = {
                 quit = "q", -- Close diff tab
                 toggle_explorer = "<leader>b", -- Toggle explorer visibility (explorer mode only)
-                next_hunk = "]c", -- Jump to next change
-                prev_hunk = "[c", -- Jump to previous change
+                next_hunk = "gj", -- Jump to next change
+                prev_hunk = "gk", -- Jump to previous change
                 next_file = "]f", -- Next file in explorer/history mode
                 prev_file = "[f", -- Previous file in explorer/history mode
                 diff_get = "do", -- Get change from other buffer (like vimdiff)
--- lua/goggin/plugins/gitsigns.lua
+++ lua/goggin/plugins/gitsigns.lua
@@ -12,6 +12,21 @@ return {
                 topdelete = { text = "‾" },
                 changedelete = { text = "~" },
             },
+            on_attach = function(bufnr)
+                local opts = { buffer = bufnr }
+
+                vim.keymap.set("n", "gj", function()
+                    gitsigns.nav_hunk("next")
+                end, vim.tbl_extend("force", opts, {
+                    desc = "Next Git chunk",
+                }))
+
+                vim.keymap.set("n", "gk", function()
+                    gitsigns.nav_hunk("prev")
+                end, vim.tbl_extend("force", opts, {
+                    desc = "Previous Git chunk",
+                }))
+            end,
         })
     end,
 }
